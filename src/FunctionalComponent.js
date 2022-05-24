467eca887c88b3504b35c581bca1ab2421be0cf5
Eca Coscarelli <ecacoscarelli@ARCA.local>
2022-05-24 11:31:30 -0300

    intro to useState

--- src/FunctionalComponent.js
+++ src/FunctionalComponent.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function FunctionalComponent() {
+    const [counter, setCounter] = useState(0)
+
   return (
     <div>
         <h3 onClick={()=>console.log("Yo!")}>Here's a functional component</h3>
+        <h2>{counter}</h2>
+        <button onClick={()=>setCounter(counter+1)}>+1</button>
+        <button onClick={()=>setCounter(counter+10)}>+10</button>
+        <button onClick={()=>setCounter(counter-1)}>-1</button>
+        <button onClick={()=>setCounter(0)}>RESET</button>
     </div>
   )
 }
